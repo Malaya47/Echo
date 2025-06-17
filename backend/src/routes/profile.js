1081803b079dc23353ab886f12354f8e27c62a07
Malaya Tiwari <sidzilegend@gmail.com>
2025-06-17 22:34:27 +0530

    update: profile edit API

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -2,8 +2,9 @@ const express = require("express");
 const profileRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     if (!user) {
@@ -13,6 +14,28 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "Error fetching profile",
+      error: error.message,
+    });
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+    await loggedInUser.save();
+    res.status(200).json({
+      message: `${loggedInUser.firstName} ${loggedInUser.lastName}'s profile updated successfully`,
+      user: loggedInUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error updating profile",
       error: error.message,
     });
   }
