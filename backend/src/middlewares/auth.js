35e603a77c9c34cd37e2a7d56d34df90ddd4dce6
Malaya Tiwari <sidzilegend@gmail.com>
2025-06-17 16:10:06 +0530

    update: made auth middleware

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const userAuth = async (req, res, next) => {
+  try {
+    // Read the token from the req cookies
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Token is not valid!!");
+    }
+
+    const decodedObj = await jwt.verify(token, "your_secret_key");
+
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: "Unauthorized access",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  userAuth,
+};
