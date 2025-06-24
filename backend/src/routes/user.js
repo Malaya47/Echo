4b6715a794a2a5699c59386b1463a56baf28959c
Malaya Tiwari <sidzilegend@gmail.com>
2025-06-24 20:57:10 +0530

    update: made connection API

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -4,21 +4,23 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_SAFE_DATA = [
+  "firstName",
+  "lastName",
+  "age",
+  "gender",
+  "photoUrl",
+  "about",
+  "skills",
+];
+
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
     const receivedRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "age",
-      "gender",
-      "photoUrl",
-      "about",
-      "skills",
-    ]);
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     if (!receivedRequests) {
       throw new Error("No requests found");
@@ -31,6 +33,45 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "An error occured while fetching requests",
+      error: error.message,
+    });
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    // loggedInUser ---> connections --> status: "accepted" + toUserId = loggedInUserId._id or fromUserId = loggedInUserId._id
+    const loggedInUser = req.user;
+
+    const connections = await ConnectionRequest.find({
+      $or: [
+        {
+          fromUserId: loggedInUser._id,
+          status: "accepted",
+        },
+        {
+          toUserId: loggedInUser._id,
+          status: "accepted",
+        },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connections.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.status(200).json({
+      message: "Connections fetched successfully",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "An error occured while fetching connections",
       error: error.message,
     });
   }
