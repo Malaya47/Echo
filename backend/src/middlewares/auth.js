981188a98b7641b293d7bf3c21afc73c056b2e66
Malaya Tiwari <sidzilegend@gmail.com>
2025-07-07 11:26:49 +0530

    update: error status if token not present

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -6,7 +6,9 @@ const userAuth = async (req, res, next) => {
     // Read the token from the req cookies
     const { token } = req.cookies;
     if (!token) {
-      throw new Error("Token is not valid!!");
+      return res.status(401).json({
+        message: "Unauthorized access",
+      });
     }
 
     const decodedObj = await jwt.verify(token, "your_secret_key");
